7b59e18897a5ef09340a8ba244d8da8a3f10a931
Kevin Seifert <kevin.j.seifert@gmail.com>
2015-10-01 18:53:24 +0000

    implemented first cut of charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #get 'pages/home'
 
   root 'pages#home'
+  
+  #support stripe payments through charges
+  resources :charges
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
