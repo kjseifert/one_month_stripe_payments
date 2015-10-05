4a5f542657d01cab099702c5b414a36cc6700ad2
Kevin Seifert <kevin.j.seifert@gmail.com>
2015-10-05 19:46:38 +0000

    added purchase view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   
   #support stripe payments through charges
   resources :charges
+  #route for showing purchases
+    resources :purchases, only: [:show]
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
