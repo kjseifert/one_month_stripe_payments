158dd093f1cf0d06500833117e5dd32f8b84fd14
Kevin Seifert <kevin.j.seifert@gmail.com>
2015-10-23 16:26:10 +0000

    Added products table and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ AdminUser.create!(email: 'admin@example.com', password: 'password', password_con
 growth_hacking = Product.create(title: "Growth Hacking", 
  subtitle: "Crash Course", author: "Mattan Griffel", 
  length: "30 minutes", author_image_name: "teacher-image.jpg", 
- price: "4.99", sku: "GROHACK1", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+ price: "4.99", sku: "GROHACK1", download_url: "https://s3.amazonaws.com/one-month-stripe-payments-kjs/pictest.jpg", 
  details: "You'll get one video", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
 
  <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
