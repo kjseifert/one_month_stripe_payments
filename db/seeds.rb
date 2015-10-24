076baaf8fa4cd9d5b612245f203870dc513649b8
Kevin Seifert <kevin.j.seifert@gmail.com>
2015-10-24 19:08:56 +0000

    Implemented subscription based product GROHACK2

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,15 @@ growth_hacking = Product.create(title: "Growth Hacking",
                                                                      }
 
 )
+
+growth_hacking = Product.create(title: "Growth Hacking Monthly",
+ subtitle: "Crash Course", author: "Mattan Griffel", download_url: "https://s3.amazonaws.com/one-month-stripe-payments-kjs/pictest.jpg", author_image_name: "teacher-image.jpg", price: "4.99", details: "You'll be charged monthly", length: "6 Months", sku: "GROHACK2", description: "COURSE XX DESCRIPTION HERE", author_description: %{
+  
+  <p><strong><%= product.author%></strong></p>
+                <p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>
+                                                                     }
+
+)
